12444c9bdc52306bea5960ce75bb403e66e541d3
Rolomza <augusto.robles.mza@gmail.com>
2024-09-18 18:56:15 -0300

    main repl

---
+++ command_help.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Welcome to the Pokedex help menu!")
+	fmt.Println("Here are your available commands:")
+	fmt.Println()
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
+	}
+	
+	fmt.Println()
+	return nil
+}
--- main.go
+++ main.go
@@ -1,69 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-var cliCommandMap map[string]cliCommand
-
-func init() {
-	cliCommandMap = map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-}
-
-type cliCommand struct {
-	name string
-	description string
-	callback func() error
-}
-
-func commandHelp() error {
-	fmt.Print("\nWelcome to the Pokedex!\n")
-	fmt.Print("Usage:\n\n")
-	for _, command := range cliCommandMap {
-		fmt.Printf("%s: %s\n", command.name, command.description)
-	}
-	fmt.Println()
-	return nil
-}
-
-func commandExit() error {
-	return nil
-}
-
 func main() {
 
-	for {
-		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-
-		command, ok := cliCommandMap[scanner.Text()]
-		if !ok {
-			fmt.Println("Error: command not found. Type 'help' to see available commands.")
-			continue
-		}
-
-		switch command.name {
-		case "help":
-			command.callback()
-			continue
-		case "exit":
-			command.callback()
-			return
-		}
-	}
+	startRepl()
 	
 }
